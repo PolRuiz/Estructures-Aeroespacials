121a67d2994871c120701700d070c1fe02541252
PolRuiz <pol.ruiz.celada@estudiantat.upc.edu>
2022-12-26 20:49:59 +0100

    Update mainA02.m

--- mainA02.m
+++ mainA02.m
@@ -3,41 +3,13 @@ clear;
 close all;
 
 %%
-s.Mass = 150; %kg
-s.AeroM = 1;
 
-hangGlider = TrussStructure(s);
+dataCreator = DataCreator();
+problemData = dataCreator.createData();
 
-hangGlider.createData();
+hangGlider = TrussStructure(problemData);
+hangGlider.computeProblem();
 
-hangGlider.computeDOFConnectivity();
-
-hangGlider.computeElementStiffnes();
-
-hangGlider.computeStiffnessMatrix();
-KG = hangGlider.KG;
-
-hangGlider.computeGlobalForceVector();
-Fext = hangGlider.Fext;
-
-hangGlider.applyBoundaryCond();
-vL = hangGlider.vectorialData.vL;
-vR = hangGlider.vectorialData.vR;
-
-
-% System resolution
-
-solverType = 'Direct'; %'Iterative' or 'Direct'
-[uL,u,R] = solveSys(hangGlider, KG, Fext, solverType);
-
-hangGlider.computeStrainStress(u, uL);
-
-
-%% POSTPROCESS
-
-% Plot deformed structure with stress of each bar
-scale = 30; % Adjust this parameter for properly visualizing the deformation
-plotBarStress3D(hangGlider,scale);
 
 %% TESTS
 
