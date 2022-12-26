600327590c4e97b26f760394d44e21f8bc7af20c
PolRuiz <pol.ruiz.celada@estudiantat.upc.edu>
2022-12-26 20:50:02 +0100

    Update tests.m

--- tests.m
+++ tests.m
@@ -2,28 +2,64 @@ classdef tests < matlab.unittest.TestCase
     
     properties (Access = private)
         tolerance = 10^-12;
+        
     end
     
     methods(Test)
         function stiffnessMatrix(testCase)
-            actualMatrix = evalin('base','KG');
-            expectedMatrix = load('StiffnessMatrix.mat','KG').KG;
+            dimensionalData = load('dimensionalData.mat').dimensionalData;
+            s.n_el = dimensionalData.n_el;
+            s.n_dof = dimensionalData.n_dof;
+            s.n_el_dof = dimensionalData.n_el_dof;
+            
+            s.Tn = load('mesh.mat').mesh.nodalConnec;            
+            s.Td = load('dofConnec.mat').DOFConnec;
+            s.Kel = load('elementStiffness.mat').elementStiffness;
+            
+            c = GlobalStiffnessMatrixComputer(s);
+            
+            actualMatrix = c.compute();
+            expectedMatrix = load('stiffnessMatrix.mat').KG;
             testCase.verifyEqual(actualMatrix,expectedMatrix);
         end
         
         function externalForce(testCase)
-            actualValue = evalin('base','Fext');
-            expectedValue = load('ExternalForce.mat','Fext').Fext;
+            dimensionalData = load('dimensionalData.mat').dimensionalData;
+            s.n = dimensionalData.n;
+            s.n_d = dimensionalData.n_d;
+            s.n_el = dimensionalData.n_el;
+            s.n_dof = dimensionalData.n_dof;
+            
+            s.mesh = load('mesh.mat').mesh;
+            s.materialData = load('materialData.mat').materialData;
+            
+            parameters = load('parameters.mat').parameters;
+            s.Wm = parameters.hangingMass;
+            s.AeroM = parameters.aeroMultiplier;
+            
+            geometricalData = load('geometricalData.mat').geometricalData;
+            
+            s.W = geometricalData.W;
+            s.H = geometricalData.H;
+            s.D1 = geometricalData.D1;
+            s.d1 = geometricalData.d1;
+            s.D2 = geometricalData.D2;
+            
+            c = GlobalForceComputer(s);
+            actualValue = c.compute();
+            expectedValue = load('externalForce.mat').Fext;
             testCase.verifyEqual(actualValue,expectedValue);
         end
         
         function directDisplacements(testCase)
-            vL = evalin('base','vL');
-            vR = evalin('base','vR');
+            vL = load('freeDOF.mat').vL;
+            vR = load('fixedDOF.mat').vR;
             
-            s.LHS = load('LHS.mat','LHS').LHS;
-            s.RHS = load('RHS.mat','RHS').RHS;
+            sys.LHS = load('LHS.mat','LHS').LHS;
+            sys.RHS = load('RHS.mat','RHS').RHS;
+            
             s.type = 'Direct';
+            s.system = sys;
             
             solver = Solver.create(s);
             uL = solver.solve();
@@ -31,7 +67,7 @@ classdef tests < matlab.unittest.TestCase
             actualValue(vL,1) = uL;
             actualValue(vR,1) = zeros(size(vR,1),1);
             
-            expectedValue = load('Displacements.mat','u').u;
+            expectedValue = load('displacements.mat','u').u;
             
             err = testCase.computeVectorError(actualValue,expectedValue);
             
@@ -43,12 +79,14 @@ classdef tests < matlab.unittest.TestCase
         end
         
         function iterativeDisplacements(testCase)
-            vL = evalin('base','vL');
-            vR = evalin('base','vR');
+            vL = load('freeDOF.mat').vL;
+            vR = load('fixedDOF.mat').vR;
             
-            s.LHS = load('LHS.mat','LHS').LHS;
-            s.RHS = load('RHS.mat','RHS').RHS;
+            sys.LHS = load('LHS.mat').LHS;
+            sys.RHS = load('RHS.mat').RHS;
+            
             s.type = 'Iterative';
+            s.system = sys;
             
             solver = Solver.create(s);
             uL = solver.solve();
@@ -56,7 +94,7 @@ classdef tests < matlab.unittest.TestCase
             actualValue(vL,1) = uL;
             actualValue(vR,1) = zeros(size(vR,1),1);
             
-            expectedValue = load('Displacements.mat','u').u;
+            expectedValue = load('displacements.mat').u;
             
             err = testCase.computeVectorError(actualValue,expectedValue);
             
