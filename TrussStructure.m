fbf1d1324a0e58c4b7746dda698817c5801c29f0
PolRuiz <pol.ruiz.celada@estudiantat.upc.edu>
2023-01-01 21:38:54 +0100

    Update TrussStructure.m

--- TrussStructure.m
+++ TrussStructure.m
@@ -12,15 +12,12 @@ classdef TrussStructure < handle
     properties (Access = private)
         DOFConnec
         elementStiffness
-
         StiffnessMatrix
-        ExternalForces
-        
+        ExternalForces        
         postprocessData
         reactions
         displacements
         problemDOFManager
-
     end
 
     methods (Access = public)
@@ -43,45 +40,42 @@ classdef TrussStructure < handle
 
     methods (Access = private)
 
-
         function computeDOFConnectivity(obj)
-            s.n_el = obj.dimensionalData.n_el;
-            s.n_nod = obj.dimensionalData.n_nod;
-            s.n_i = obj.dimensionalData.n_i;
-            s.Tn = obj.mesh.nodalConnec;
-            
+            dD = obj.dimensionalData;
+            s.n_el  = dD.n_el;
+            s.n_nod = dD.n_nod;
+            s.n_i   = dD.n_i;
+            s.Tn    = obj.mesh.nodalConnec;            
             c = DOFConnecter(s);
             obj.DOFConnec = c.compute();
         end
 
         function computeElementStiffnes(obj)
-            s.n_d = obj.dimensionalData.n_d;
-            s.n_el = obj.dimensionalData.n_el;
-            s.mesh = obj.mesh;
-            s.materialData = obj.materialData;
-            
+            dD  = obj.dimensionalData;
+            s.n_d           = dD.n_d;
+            s.n_el          = dD.n_el;
+            s.n_el_dof      = dD.n_el_dof;
+            s.mesh          = obj.mesh;
+            s.materialData  = obj.materialData;            
             c = ElementStiffnessComputer(s);
             obj.elementStiffness = c.compute();
         end
 
         function computeStiffnessMatrix(obj)
-
-            s.n_dof = obj.dimensionalData.n_dof;
-            s.n_el = obj.dimensionalData.n_el;
-            s.n_el_dof = obj.dimensionalData.n_el_dof;
-
-            s.Tn = obj.mesh.nodalConnec;            
-            s.Kel = obj.elementStiffness;
-            s.Td = obj.DOFConnec;
-
+            dD = obj.dimensionalData;
+            s.n_dof     = dD.n_dof;
+            s.n_el      = dD.n_el;
+            s.n_el_dof  = dD.n_el_dof;
+            s.Tn        = obj.mesh.nodalConnec;            
+            s.Kel       = obj.elementStiffness;
+            s.Td        = obj.DOFConnec;
             c = GlobalStiffnessMatrixComputer(s);
             obj.StiffnessMatrix = c.compute();             
         end
 
         function computeGlobalForceVector(obj)
             s.mesh = obj.mesh;
-            s.materialData = obj.materialData;
-            
+            s.materialData = obj.materialData;            
             gD = obj.geometricalData;
             s.W  = gD.W;
             s.H  = gD.H;
@@ -91,11 +85,9 @@ classdef TrussStructure < handle
             s.n     = obj.dimensionalData.n;
             s.n_d   = obj.dimensionalData.n_d;
             s.n_el  = obj.dimensionalData.n_el;
-            s.n_dof = obj.dimensionalData.n_dof;
-            
-            s.Wm    = obj.parameters.hangingMass;
-            s.AeroM = obj.parameters.aeroMultiplier;
-            
+            s.n_dof = obj.dimensionalData.n_dof;            
+            s.hMass = obj.parameters.hangingMass;
+            s.AeroM = obj.parameters.aeroMultiplier;            
             c = GlobalForceComputer(s);
             obj.ExternalForces = c.compute();
         end
@@ -110,42 +102,34 @@ classdef TrussStructure < handle
             solverType = 'Direct';
             newMatrices = obj.problemDOFManager.splitMatrix(obj.StiffnessMatrix);
             newVectors = obj.problemDOFManager.splitVector(obj.ExternalForces);
-
             s.type = solverType;
             s.system = obj.problemDOFManager.constructSystem(newMatrices,newVectors);
-
             solver = Solver.create(s);
-
             uL = solver.solve();
             uR = obj.fixNod(:,3);
             RR = newMatrices.matRR*uR+newMatrices.matRL*uL-newVectors.vecR;
-
-
-            sol.displacements = obj.problemDOFManager.joinVector(uL,uR);
-            sol.reactions = obj.problemDOFManager.joinVector(0,RR);
-
-            obj.resultData = sol;
+            obj.displacements = obj.problemDOFManager.joinVector(uL,uR);
+            obj.reactions = obj.problemDOFManager.joinVector(0,RR);
         end
 
         function computeStrainStress(obj)
-            s.n_d   = obj.dimensionalData.n_d;
-            s.n_i   = obj.dimensionalData.n_i;
-            s.n_nod = obj.dimensionalData.n_nod;
-            s.n_el  = obj.dimensionalData.n_el;             
-            s.mesh = obj.mesh;
-            s.materialData = obj.materialData;            
-            s.DOFConnec = obj.DOFConnec;
-            s.u = obj.resultData.displacements;
-            
+            dD = obj.dimensionalData;
+            s.n_d   = dD.n_d;
+            s.n_i   = dD.n_i;
+            s.n_nod = dD.n_nod;
+            s.n_el  = dD.n_el;             
+            s.mesh  = obj.mesh;                      
+            s.Td    = obj.DOFConnec;
+            s.mData = obj.materialData;  
+            s.u     = obj.displacements;            
             c = StrainStressComputer(s);
             obj.postprocessData = c.compute();
         end
 
-        function plotPostprocess(obj)
-            % Plot deformed structure with stress of each bar
-            scale = 30; % Adjust this parameter for properly visualizing the deformation
+        function plotPostprocess(obj)            
+            scale = 30;
             s.mesh = obj.mesh;
-            s.u = obj.resultData.displacements;
+            s.u = obj.displacements;
             s.sig = obj.postprocessData.stress;            
             plotBarStress3D(s,scale);
         end
@@ -155,7 +139,7 @@ classdef TrussStructure < handle
     methods (Access = private)
 
         function init(obj, cParams)
-            obj.parameters = cParams.parameters ; % mass hanging from bar
+            obj.parameters = cParams.parameters;
             obj.geometricalData = cParams.geometricalData;
             obj.mesh = cParams.mesh;
             obj.materialData = cParams.materialData;
